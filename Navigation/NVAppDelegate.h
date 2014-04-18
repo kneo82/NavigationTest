cc36b3cd625042a890ed1d97ff2c8446147fc794
kneo82 <kneo82@gmail.com>
2014-04-18 12:03:33 +0300

    Initial Commit

---
+++ Navigation/NVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NVAppDelegate.h
+//  Navigation
+//
+//  Created by Vitaliy Voronok on 4/18/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Navigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Navigation
+//
+//  Created by Vitaliy Voronok on 4/18/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NVAppDelegate class]));
+    }
+}
